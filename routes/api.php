ca57f168bb6ecfd4a9ec26b8a2e01a67e1ccb6b7
Surya_g <surya@amypo.in>
2025-04-22 15:52:37 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/register', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
