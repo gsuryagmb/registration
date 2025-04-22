2125e2585af2c01b46c8844eea23ad099c1be589
Surya_g <surya@amypo.in>
2025-04-22 12:50:05 +0530

    Auth controller

---
+++ app/Http/Controllers/Authentication.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class Authentication extends Controller
+{
+    //
+    public function login()
+    {
+
+    }
+}
