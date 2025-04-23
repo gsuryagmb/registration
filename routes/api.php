bb80ee6b4c521871fb761894bfed4fc4fecf00cc
Muzakir-amypo <m.ahamed@amypo.in>
2025-04-23 09:33:52 +0530

    Up

--- routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\AuthController;
-use Illuminate\Support\Facades\Route;
-
-Route::post('/api/register', [AuthController::class, 'register']);
-Route::post('/api/login', [AuthController::class, 'login']);
